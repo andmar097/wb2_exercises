c00a2101a640cab851a645ebe47d2612c228d9a5
Andrea Martinez <mandrea781@outlook.com>
2024-04-24 11:57:36 -0500

    completed the area of a rectangle exercise

---
+++ area_of_rectangle.js
@@ -0,0 +1,9 @@
+//variables created as inputs for calculation
+let length = 5;
+let width = 10;
+
+//do our calculations for the variables above
+let area = length * width;
+
+//output the results to the console
+console.log ("the area of the rectangle is: " + area);
