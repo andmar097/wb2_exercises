141f789d26b16d07800e2971bf90d3baf27fcf4b
Andrea Martinez <mandrea781@outlook.com>
2024-04-24 11:48:19 -0500

    completed networth js and added area of rectangle file

---
+++ net_worth.js
@@ -0,0 +1,9 @@
+//variables created as inputs for calculation
+let assets = 5000.00;
+let debts = 2000.00;
+
+//do our calculations for the variables above
+let totalNetWorth = assets - debts;
+
+//output the results to the console
+console.log("the total of your net worth is " + totalNetWorth)
