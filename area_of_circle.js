791b281ba15263f871cb9282bdd1a55218d6b333
Andrea Martinez <mandrea781@outlook.com>
2024-04-24 13:32:03 -0500

    completed area of a circle

---
+++ area_of_circle.js
@@ -0,0 +1,9 @@
+//variables created as inputs for calculation
+let pi= 3.14
+let radius = 20
+
+//do our calculations for the variables above
+let areaOfCircle = pi * radius ** 2
+
+//output the results to the console
+console.log("the area of the circle is " + areaOfCircle)
