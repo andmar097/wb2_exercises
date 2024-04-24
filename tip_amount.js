f351e1b4688d6e77b8929a29c25a1f47b2a49dae
Andrea Martinez <mandrea781@outlook.com>
2024-04-24 13:29:10 -0500

    completed tip amount js and added area of circle file

---
+++ tip_amount.js
@@ -0,0 +1,11 @@
+//variables created as inputs for calculation
+let tipPercent = 0.18;
+let bill = 65.84;
+
+
+//do our calculations for the variables above
+let tip = bill * tipPercent;
+
+//output the results to the console
+console.log("your tip is " + tip);
+
