c8615bae9d7feb435d84b9543d0c24d4cea0dcc0
Andrea Martinez <mandrea781@outlook.com>
2024-04-25 13:36:32 -0500

    completed conversions assignment

---
+++ conversation_tests.js
@@ -0,0 +1,23 @@
+"use strict"
+
+//the variables we are going to work with
+let a = "101.1 ";
+let b = "55";
+let c = "402 Stevens"
+let d = "Number 5  ";
+
+//using parseInt for each variable and displaying the results
+
+console.log("parseInt: " + parseInt(a) + ", " + parseInt(b) + ", " + parseInt(c) + ", " + parseInt(d));
+
+//using parseFloat for each variable and displaying the results
+
+console.log("parseFloat: " + parseFloat(a) + ", " + parseFloat(b) + ", " + parseFloat(c) + ", " + parseFloat(d));
+
+//using Number for each variable and displaying the results
+
+console.log("Number: " + Number(a) + ", " + Number(b) + ", " + Number(c) + ", " + Number(d));
+
+//using unary for each variable and displaying the results
+
+console.log("Unary operator (+): " + +a + ", " + +b + ", " + +c + ", " + +d);
