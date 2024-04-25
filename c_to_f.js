df833c59c87b29d349d541b86e678c37e809ed79
Andrea Martinez <mandrea781@outlook.com>
2024-04-25 09:38:19 -0500

    completed c to f exercise

---
+++ c_to_f.js
@@ -0,0 +1,5 @@
+let celsius = 200
+
+let farenheit = ((9 * celsius) / 5) + 32;
+
+console.log(" the temperature of " + celsius + " degrees celsius is equal to " + farenheit + " degress farenheit ");
